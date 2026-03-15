a1b514929bad3e58755fe295baacc45f690a5064
claroca07 <anaclara123.limaandre@gmail.com>
2026-03-15 14:54:01 -0300

    feat: mudança de paleta de cores; animação de digitação implementada

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import { TypeAnimation } from "react-type-animation"
 import { StarsBackground } from "./components/animate-ui/components/backgrounds/stars"
 
 function App() {
@@ -8,8 +9,26 @@ function App() {
 
       <div className="relative z-10">
         <Navbar />
-        <section className="flex justify-center">
-        <h1 className="text-white mt-10 text-4xl font-bold">Hi, I'm <span className="text-glow">Ana Clara</span></h1>
+        <section className="flex flex-col items-center justify-center text-center">
+          <h1 className="text-white mt-10 text-4xl font-bold">Hi, I'm <span className="text-glow">Ana Clara</span></h1>
+          <div className="text-typed mt-4">
+            <TypeAnimation
+              sequence={[
+                'I am a Front-end Developer',
+                1000,
+                'I am a Web Designer',
+                1000,
+                'I am a Artist',
+                1000,
+                'I am here to help you',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: '2em', display: 'inline-block' }}
+              repeat={Infinity}
+            />
+          </div>
         </section>
       </div>
     </div>
--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/img/logo.png"
 
 function Navbar() {
     return(
-        <nav className="navbar flex justify-between items-center bg-black/30 backdrop-blur-sm py-3">
+        <nav className="navbar flex justify-between items-center bg-black/30 backdrop-blur-sm py-5">
             <div className="flex items-center gap-2">
                 <img className="w-32" src={logo} alt="Logo" />
             </div>
@@ -11,7 +11,7 @@ function Navbar() {
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Projects</a></li>
-                <li><a href="#contacts">Contacts</a></li>
+                <li><a href="#contacts">Contact</a></li>
             </ul>
         </nav>
     )
