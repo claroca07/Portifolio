148143d4fecf71fdb54c1a287dcc7dc9864beb48
claroca07 <anaclara123.limaandre@gmail.com>
2026-03-15 15:25:58 -0300

    feat: finalização da tela inicial, v-desktop

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -9,16 +9,16 @@ function App() {
 
       <div className="relative z-10">
         <Navbar />
-        <section className="flex flex-col items-center justify-center text-center">
-          <h1 className="text-white mt-10 text-4xl font-bold">Hi, I'm <span className="text-glow">Ana Clara</span></h1>
+        <section className="flex flex-col items-center justify-center text-center mt-20">
+          <h1 className="text-[var(--light-color)] mt-10 text-4xl font-bold">hi, I'm <span className="text-glow">Ana Clara</span></h1>
           <div className="text-typed mt-4">
             <TypeAnimation
               sequence={[
-                'I am a Front-end Developer',
+                'I am a front-end developer',
                 1000,
-                'I am a Web Designer',
+                'I am a web designer',
                 1000,
-                'I am a Artist',
+                'I am a artist',
                 1000,
                 'I am here to help you',
                 1000
@@ -29,6 +29,10 @@ function App() {
               repeat={Infinity}
             />
           </div>
+          <div className="mt-6 h-1 w-96 bg-gradient-to-r from-transparent via-blue-800 to-transparent"></div>
+          <p className="mt-4 font-medium text-2xl text-[var(--light-color)] max-w-2xl leading-relaxed">
+            Passionate about creating beautiful and functional web experiences, transforming ideas into digital reality.
+          </p>
         </section>
       </div>
     </div>
--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import logo from "../assets/img/logo.png"
 
 function Navbar() {
     return(
-        <nav className="navbar flex justify-between items-center bg-black/30 backdrop-blur-sm py-5">
+        <nav className="navbar flex justify-between items-center bg-gradient-to-b from-black/60 to-transparent backdrop-blur-sm py-5">
             <div className="flex items-center gap-2">
                 <img className="w-32" src={logo} alt="Logo" />
             </div>
             <ul className="flex gap-6">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contacts">Contact</a></li>
+                <li><a href="#home">home</a></li>
+                <li><a href="#about">about</a></li>
+                <li><a href="#skills">skills</a></li>
+                <li><a href="#projects">projects</a></li>
+                <li><a href="#contacts">contact</a></li>
             </ul>
         </nav>
     )
