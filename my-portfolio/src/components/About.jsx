09035d8d5a21059d04a9d554ab66d3d1bd477af5
claroca07 <anaclara123.limaandre@gmail.com>
2026-03-16 12:23:07 -0300

    feat: estrutura inicial da parte about me

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -1,9 +1,17 @@
 function About() {
-    return(
-        <section className="flex text-[var(--light-color)]">
-            <h1>Forma</h1>
-            <div className="card"></div>
-            <div className="card"></div>
+    return (
+        <section className="text-[var(--light-color)] flex flex-col justify-center items-center gap-6 mt-20">
+            <h1 className="title text-3xl font-medium">Education</h1>
+            <div className="flex gap-10 my-6 flex-col sm:flex-row">
+                <div className="card">
+                    <h2>Technical Degree in Systems Development</h2>
+                    <h3>Etec Sales Gomes — 2023 - 2025</h3>
+                </div>
+                <div className="card">
+                    <h2>Systems Analysis and Development — Associate Degree</h2>
+                    <h3>IFSP — present - 2028</h3>
+                </div>
+            </div>
         </section>
     )
 }
