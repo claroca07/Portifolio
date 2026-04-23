856971b64730913d755174ad3136d0752f6ccfa3
claroca07 <anaclara123.limaandre@gmail.com>
2026-04-23 11:41:59 -0300

    feat: habilidades

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -1,7 +1,73 @@
-function Skills(){
-    return(
-        <section className="skills">
-            <h1>Skills</h1>
+function Skills() {
+    return (
+        <section className="skills flex flex-col justify-center items-center gap-6 mt-20">
+            <h1 className="title text-3xl font-medium text-center">My <span className="text-glow">Skills</span></h1>
+            <main className="flex flex-wrap justify-center gap-6 my-10">
+                <article className="und">
+                    <i class="devicon-css3-plain"></i>
+                    <h3>CSS</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-django-plain"></i>
+                    <h3>Django</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-figma-plain"></i>
+                    <h3>Figma</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-git-plain"></i>
+                    <h3>Git</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-html5-plain"></i>
+                    <h3>HTML</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-javascript-plain"></i>
+                    <h3>JavaScript</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-mysql-original"></i>
+                    <h3>MySQL</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-nodejs-plain"></i>
+                    <h3>Node.js</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-php-plain"></i>
+                    <h3>PHP</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-python-plain"></i>
+                    <h3>Python</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-react-original"></i>
+                    <h3>React</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-selenium-original"></i>
+                    <h3>Selenium</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-tailwindcss-original"></i>
+                    <h3>Tailwind CSS</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-typescript-plain"></i>
+                    <h3>TypeScript</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-jest-plain"></i>
+                    <h3>Jest</h3>
+                </article>
+                <article className="und">
+                    <i class="devicon-bootstrap-plain"></i>
+                    <h3>Bootstrap</h3>
+                </article>
+            </main>
         </section>
     )
 }
